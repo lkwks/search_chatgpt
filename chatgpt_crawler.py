62116e9e96dfc24162bce26cd77b09ff47de43d7
Kangwook Lee <lkwks@naver.com>
2022-12-05 04:35:08 +0900

    Update chatgpt_crawler.py

--- chatgpt_crawler.py
+++ chatgpt_crawler.py
@@ -3,18 +3,15 @@ from os import environ
 import time
 
 # Authenticate to Twitter
-try:
-    auth = tweepy.OAuthHandler(environ["consumer_key"], environ["consumer_secret"])
-    auth.set_access_token(environ["access_token"], environ["access_token_secret"])
-except:
-    raise
+auth = tweepy.OAuthHandler(environ["consumer_key"], environ["consumer_secret"])
+auth.set_access_token(environ["access_token"], environ["access_token_secret"])
 
 # Create API object
 api = tweepy.API(auth)
 
 # Define the search query and the maximum number of tweets to retrieve
 query = "ChatGPT min_retweets:10"
-max_tweets = 100
+max_tweets = 20
 
 # Search for tweets matching the query and store the results
 tweets = {}
@@ -23,9 +20,9 @@ for lang in ["en", "ko"]:
 
 
 # Get the list of tweet ids that have already been tweeted
-tweeted_ids = []
-for tweet in tweepy.Cursor(api.user_timeline).items(10):
-    tweeted_ids.append(tweet.id)
+my_tweets = []
+for tweet in tweepy.Cursor(api.user_timeline).items(100):
+    my_tweets.append(tweet.text)
 
 # Iterate over the tweets and tweet each one to the private account
 # if it hasn't already been tweeted and if it's written in English
@@ -33,8 +30,14 @@ for tweet in tweepy.Cursor(api.user_timeline).items(10):
 i = 1
 for lang in ["en", "ko"]:
     for tweet in tweets[lang]:
-        if tweet.id not in tweeted_ids and tweet.lang == lang:
+        for my_tweet in my_tweets:
+            if f"https://twitter.com/twitter/statuses/{tweet.id}" in my_tweet:
+                break
+        else:
             time.sleep(5)
-            api.update_status(status=f"{tweet.text[:50]}... https://twitter.com/twitter/statuses/{tweet.id}")
-            i += 1
-            print(f"tweeting {i}th tweet completed.")
+            try:
+                api.update_status(status=f"https://twitter.com/twitter/statuses/{tweet.id}")
+                i += 1
+                print(f"tweeting {i}th tweet completed.")
+            except Exception as e:
+                print(e)
