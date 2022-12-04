aedc956427335b54965aec843bea241975e3d776
Kangwook Lee <lkwks@naver.com>
2022-12-05 04:26:25 +0900

    Update chatgpt_crawler.py

--- chatgpt_crawler.py
+++ chatgpt_crawler.py
@@ -30,8 +30,11 @@ for tweet in tweepy.Cursor(api.user_timeline).items(10):
 # Iterate over the tweets and tweet each one to the private account
 # if it hasn't already been tweeted and if it's written in English
 
+i = 1
 for lang in ["en", "ko"]:
     for tweet in tweets[lang]:
         if tweet.id not in tweeted_ids and tweet.lang == lang:
             time.sleep(5)
-            api.update_status(status=f"{tweet.text[:50]}, URL: https://twitter.com/twitter/statuses/{tweet.id}")
+            api.update_status(status=f"{tweet.text[:50]}... https://twitter.com/twitter/statuses/{tweet.id}")
+            i += 1
+            print(f"tweeting {i}th tweet completed.")
