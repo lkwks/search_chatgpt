6d193f707406ca8a2f875a66faab39d131c0bae3
Kangwook Lee <lkwks@naver.com>
2022-12-12 04:52:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ min_retweets = int(environ["min_retweets"])
 tweets_to_update = int(environ["tweets_to_update"])
 get_my_tweets_n = int(environ["get_my_tweets_n"])
 lang = environ["search_lang"]
+now_date = datetime.datetime.now()
+search_date = datetime.datetime(now_date.year, now_date.month, now_date.day-1).strftime('%Y-%m-%d')
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(environ["consumer_key"], environ["consumer_secret"])
@@ -15,26 +17,17 @@ auth.set_access_token(environ["access_token"], environ["access_token_secret"])
 # Create API object
 api = tweepy.API(auth)
 
-# Define the search query and the maximum number of tweets to retrieve
-now_date = datetime.datetime.now()
-search_date = datetime.datetime(now_date.year, now_date.month, now_date.day-1).strftime('%Y-%m-%d')
-query = f"{search_keyword} min_retweets:{min_retweets} since:{search_date}"
-
-# Search for tweets matching the query and store the results
-tweets = tweepy.Cursor(api.search_tweets, q=query, lang=lang).items(tweets_to_update)
-
 # Get the list of tweet ids that have already been tweeted
 my_tweets = []
 for tweet in tweepy.Cursor(api.user_timeline).items(get_my_tweets_n):
     tco_url = re.search(r'https://t\.co/[a-zA-Z0-9]+', tweet.text).group()
     my_tweets.append(requests.get(tco_url, allow_redirects=True).url)
-    print(my_tweets[-1])
-    break
+
+# Define the search query and the maximum number of tweets to retrieve
+query = f"{search_keyword} min_retweets:{min_retweets} since:{search_date}"
 
 # Iterate over the tweets and tweet each one to the private account
-# if it hasn't already been tweeted and if it's written in English
-i = 1
-for tweet in tweets:
+for tweet in tweepy.Cursor(api.search_tweets, q=query, lang=lang).items(tweets_to_update):
     for my_tweet in my_tweets:
         if f"{tweet.id}" in my_tweet:
             break
@@ -42,7 +35,5 @@ for tweet in tweets:
         time.sleep(5)
         try:
             api.update_status(status=f"https://twitter.com/twitter/statuses/{tweet.id}")
-            print(f"tweeting {i}th tweet completed.")
-            i += 1
         except Exception as e:
             print(e)
