51eaffe048580f34c89e51897e4acb0ce4409607
Kangwook Lee <lkwks@naver.com>
2022-12-12 01:13:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ search_keyword = environ["search_keyword"]
 min_retweets = int(environ["min_retweets"])
 tweets_to_update = int(environ["tweets_to_udpate"])
 get_my_tweets_n = int(environ["get_my_tweets_n"])
+lang = environ["search_lang"]
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(environ["consumer_key"], environ["consumer_secret"])
@@ -20,9 +21,7 @@ search_date = datetime.datetime(now_date.year, now_date.month, now_date.day-1).s
 query = f"{search_keyword} min_retweets:{min_retweets} since:{search_date}"
 
 # Search for tweets matching the query and store the results
-tweets = {}
-for lang in ["en", "ko"]:
-    tweets[lang] = tweepy.Cursor(api.search_tweets, q=query, lang=lang).items(tweets_to_update)
+tweets = tweepy.Cursor(api.search_tweets, q=query, lang=lang).items(tweets_to_update)
 
 # Get the list of tweet ids that have already been tweeted
 my_tweets = []
@@ -32,16 +31,14 @@ for tweet in tweepy.Cursor(api.user_timeline).items(get_my_tweets_n):
 # Iterate over the tweets and tweet each one to the private account
 # if it hasn't already been tweeted and if it's written in English
 i = 1
-for lang in ["en", "ko"]:
-    for tweet in tweets[lang]:
-        for my_tweet in my_tweets:
-            if f"https://twitter.com/twitter/statuses/{tweet.id}" in my_tweet:
-                break
-        else:
-            time.sleep(5)
-            try:
-                api.update_status(status=f"https://twitter.com/twitter/statuses/{tweet.id}")
-                print(f"tweeting {i}th tweet completed.")
-                i += 1
-            except Exception as e:
-                print(e)
+for my_tweet in my_tweets:
+    if f"https://twitter.com/twitter/statuses/{tweet.id}" in my_tweet:
+        break
+else:
+    time.sleep(5)
+    try:
+        api.update_status(status=f"https://twitter.com/twitter/statuses/{tweet.id}")
+        print(f"tweeting {i}th tweet completed.")
+        i += 1
+    except Exception as e:
+        print(e)
