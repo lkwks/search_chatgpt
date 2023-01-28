5deb01a1ab9b067004e744ad4c33bfce5566e183
Kangwook Lee <lkwks@naver.com>
2023-01-28 10:26:05 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from os import environ
 import tweepy, datetime, requests, re
-from bs4 import BeautifulSoup
 
 
 def tweet_update(api, msg: str) -> None:
@@ -22,31 +23,39 @@ def scrape_page():
     for tweet in tweepy.Cursor(api.user_timeline).items(100):
         tco_url_search = re.search(r'https://t\.co/[a-zA-Z0-9]+', tweet.text)
         if tco_url_search:
-            try: 
-                my_tweets.append(get_no(requests.get(tco_url_search.group(), allow_redirects=True, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/605.1.15',}).url))
-            except Exception as e:
-                print(e)
-    
-    html_txt = requests.get(environ["site_url"]).text
-    print(len(html_txt))
-    soup = BeautifulSoup(html_txt, 'html.parser')
-
-    for elem in soup.select('#container > section:nth-of-type(1) > article:nth-of-type(2) > div:nth-of-type(2) > table > tbody > tr'):
-        written_date = elem.select_one('td:nth-of-type(5)')['title']
-        if written_date == "": continue
+            my_tweets.append(get_no(requests.get(tco_url_search.group(), allow_redirects=True).url))
         
-        href = elem.select_one('td:nth-of-type(3) > a:nth-of-type(1)')['href']
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    options.add_argument("lang=ko_KR")
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    options.add_argument("--no-sandbox")
+
+    driver = webdriver.Chrome('chromedriver', chrome_options=options)
+    driver.implicitly_wait(3)    
+    
+    driver.get(environ["site_url"])
+    driver.maximize_window()    
+
+    for elem in driver.find_elements(By.XPATH, '//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr'):
+        written_date = elem.find_element(By.XPATH, 'td[5]').get_attribute('title')
+        if written_date == "": continue
+            
+        href = f"{elem.find_element(By.XPATH, './td[3]/a[1]').get_attribute('href')}"
         article_n = get_no(href)
         if article_n in my_tweets or article_n == -1: continue
-    
+        
         diff = datetime.datetime.now() - datetime.datetime.strptime(str(written_date), "%Y-%m-%d %H:%M:%S")
         if diff > datetime.timedelta(hours=24): continue
-    
+        
         if "search_keyword" in environ:
-            if environ["search_keyword"] not in elem.select_one('td:nth-of-type(3) > a:nth-of-type(1)').text: continue
-            if len(elem.select_one('td:nth-of-type(3) > a:nth-of-type(1)').text) > 20: continue                
-                
+            if environ["search_keyword"] not in elem.find_element(By.XPATH, './td[3]/a[1]').text: continue
+            if len(elem.find_element(By.XPATH, './td[3]/a[1]').text) > 20: continue
+        
         tweet_update(api, href)
   
+    driver.quit()
 
 scrape_page()
